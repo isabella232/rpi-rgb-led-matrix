9aa77b63c2f509ec66613edce333b47a8b49ab3d
Chad Pytel <chad@thoughtbot.com>
2016-12-23 14:07:47 -0500

    Make drawing image method

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -21,25 +21,90 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
-static void DrawOnCanvas(Canvas *canvas) {
+struct Pixel {
+  Pixel() : red(0), green(0), blue(0){}
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+};
+
+struct Image {
+  Image() : width(-1), height(-1), image(NULL) {}
+  ~Image() { Delete(); }
+  void Delete() { delete [] image; Reset(); }
+  void Reset() { image = NULL; width = -1; height = -1; }
+  inline bool IsValid() { return image && height > 0 && width > 0; }
+  const Pixel &getPixel(int x, int y) {
+    static Pixel black;
+    if (x < 0 || x >= width || y < 0 || y >= height) return black;
+    return image[x + width * y];
+  }
+
+  int width;
+  int height;
+  Pixel *image;
+};
+
+char *ReadLine(FILE *f, char *buffer, size_t len) {
+  char *result;
+  do {
+    result = fgets(buffer, len, f);
+  } while (result != NULL && result[0] == '#');
+  return result;
+}
+
+Image * LoadPPM(const char *filename) {
+  FILE *f = fopen(filename, "r");
+  // check if file exists
+  if (f == NULL && access(filename, F_OK) == -1) {
+    fprintf(stderr, "File \"%s\" doesn't exist\n", filename);
+  }
+  char header_buf[256];
+  const char *line = ReadLine(f, header_buf, sizeof(header_buf));
+#define EXIT_WITH_MSG(m) { fprintf(stderr, "%s: %s |%s", filename, m, line); \
+    fclose(f); return NULL; }
+  if (sscanf(line, "P6 ") == EOF)
+    EXIT_WITH_MSG("Can only handle P6 as PPM type.");
+  line = ReadLine(f, header_buf, sizeof(header_buf));
+  int new_width, new_height;
+  if (!line || sscanf(line, "%d %d ", &new_width, &new_height) != 2)
+    EXIT_WITH_MSG("Width/height expected");
+  int value;
+  line = ReadLine(f, header_buf, sizeof(header_buf));
+  if (!line || sscanf(line, "%d ", &value) != 1 || value != 255)
+    EXIT_WITH_MSG("Only 255 for maxval allowed.");
+  const size_t pixel_count = new_width * new_height;
+  Pixel *new_image = new Pixel [ pixel_count ];
+  if (fread(new_image, sizeof(Pixel), pixel_count, f) != pixel_count) {
+    line = "";
+    EXIT_WITH_MSG("Not enough pixels read.");
+  }
+#undef EXIT_WITH_MSG
+  fclose(f);
+  fprintf(stderr, "Read image '%s' with %dx%d\n", filename, new_width, new_height);
+  Image *image = new Image;
+  image->image = new_image;
+  image->width = new_width;
+  image->height = new_height;
+
+  image->image = new_image;
+  return image;
+}
+
+static void DrawOnCanvas(Canvas *canvas, int start_position, const char *filename) {
   /*
    * Let's create a simple animation. We use the canvas to draw
    * pixels. We wait between each step to have a slower animation.
    */
   canvas->Fill(0, 0, 255);
 
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
-    if (interrupt_received)
-      return;
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y,
-                     255, 0, 0);
-    usleep(1 * 1000);  // wait a little to slow down things.
+  Image * image = LoadPPM(filename);
+
+  for (int x = 0; x < image->width; ++x) {
+    for (int y = 0; y < image->height; ++y) {
+      const Pixel &p = image->getPixel(x, y);
+      canvas->SetPixel(x + start_position, y, p.red, p.green, p.blue);
+    }
   }
 }
 
@@ -60,11 +125,15 @@ int main(int argc, char *argv[]) {
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
 
-  DrawOnCanvas(canvas);    // Using the canvas.
+  DrawOnCanvas(canvas, 0, "/home/pi/tb/rpi-rgb-led-matrix/examples-api-use/images/eve.ppm");
+  DrawOnCanvas(canvas, 33, "/home/pi/tb/rpi-rgb-led-matrix/examples-api-use/images/optimus.ppm");
+  DrawOnCanvas(canvas, 65, "/home/pi/tb/rpi-rgb-led-matrix/examples-api-use/images/walle.ppm");
+  DrawOnCanvas(canvas, 97, "/home/pi/tb/rpi-rgb-led-matrix/examples-api-use/images/bender.ppm");
+  sleep(5);
 
   // Animation finished. Shut down the RGB matrix.
-  //canvas->Clear();
-  //delete canvas;
+  canvas->Clear();
+  delete canvas;
 
   return 0;
 }
